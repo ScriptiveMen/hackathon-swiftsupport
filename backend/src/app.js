6199b1cc60f0cf23f5a08e7674cdee47b7f289f6
ScriptiveMen <i.satyakumarram@gmail.com>
2026-05-02 14:49:57 +0530

    feat: implementation of GenAI

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes.js");
 const ticketRoutes = require("./routes/ticket.routes.js");
+const aiRoutes = require("./routes/ai.routes.js");
 
 const app = express();
 app.use(express.json());
@@ -12,5 +13,6 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tickets", ticketRoutes);
+app.use("/api/ai", aiRoutes);
 
 module.exports = app;
---
+++ backend/src/controllers/ai.controller.js
@@ -0,0 +1,8 @@
+async function aiResponse(req, res) {}
+
+async function aiSuggestion(req, res) {}
+
+module.exports = {
+    aiResponse,
+    aiSuggestion,
+};
